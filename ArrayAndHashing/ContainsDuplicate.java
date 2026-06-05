b97f7ad9171106133c1b28e031fbc41cab501dd9
JaVi <javairialateef627@gmail.com>
2026-06-05 11:36:19 +0500

    line removed

--- ArrayAndHashing/ContainsDuplicate.java
+++ ArrayAndHashing/ContainsDuplicate.java
@@ -5,7 +5,6 @@ class ContainsDuplicate {
     HashMap<Integer,Boolean> hashmap=new HashMap<>();
     for(int i=0;i<nums.length;i++){
         if(hashmap.containsKey(nums[i])){
-            System.out.println("hey here");
             return true;
         }
         hashmap.put(nums[i],false);
